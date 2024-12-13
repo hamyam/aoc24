90f008496e50d8ea65abe0178618dc28fb0be170
jannik radix <jannikradix@gmail.com>
2024-12-13 22:33:45 +0100

    day 12 had me cornered for too long.

--- day12/code12.py
+++ day12/code12.py
@@ -1,3 +1,5 @@
+import functools
+
 def parseInput(filename='input.dat'):
     m = []
     with open(filename, 'r') as f:
@@ -27,6 +29,7 @@ def part1(m):
                 visited.add((i, j))
                 stack.append((i,j))
                 plot.append((i,j))
+                t = m[i][j]
                 
             while stack:
                 x, y = stack.pop()
@@ -40,30 +43,60 @@ def part1(m):
                             plot.append((nx, ny))
                             stack.append((nx,ny))
                 
-            plots.append(plot)
+            plots.append({
+                "type":t,
+                "tiles":plot
+                })
     
     sum = 0 
     new_sum = 0
-    for tiles in plots:
-        pi = Plot(tiles)
+    for plot in plots:
+        tiles = plot.get("tiles")
+        t = plot.get("type")
+        pi = Plot(t, tiles)
         sum += pi.price
         new_sum += pi.new_price
+        # print(pi.outerTiles)
+        
+        print(pi)
+        
+
         # print(tiles, pi.unique_sides)
-        print(pi.area, pi.unique_sides, pi.new_price)
+        # print(pi.area, pi.unique_sides, pi.new_price)
+        # break
     
     print('sum of all tiles is ', sum)
     print('new sum of all tiles is ', new_sum)
     
 class Plot:
-    def __init__(self, tiles):
+    def __init__(self,t, tiles):
+        self.type = t
         self.tiles = tiles
+        self.inner = []
+        self.outer = []
+        
+    @functools.cached_property
+    def outerTiles(self):
+        outer = []
+        stack = self.tiles.copy()
+        
+        while stack:
+            x, y = stack.pop()
+            if (x, y+1) in self.tiles and (x, y-1) in self.tiles and (x+1, y) in self.tiles and (x-1, y) in self.tiles:
+                pass
+            else:
+                outer.append((x,y))
+        return outer
+            
+    def __str__(self):
+        return f"Plot type {self.type} with area {self.area:4d}, perim {self.perimeter:4d} sides: {self.unique_sides:4d} inner: {len(self.inner):4d} outer: {len(self.outer):4d}"
 
 
     @property
     def area(self):
         return len(self.tiles)
 
-    @property
+    @functools.cached_property
     def perimeter(self):
         p = 0 
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -80,31 +113,73 @@ class Plot:
     def price(self):
         return self.area * self.perimeter
                 
-    @property
+    @functools.cached_property
     def unique_sides(self):
-        ii = set()
-        jj = set()
-        
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        outeredges = []
+        inneredges = []
+        edges = []
         for x, y in self.tiles:
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if (nx, ny) in self.tiles:
-                    pass
-                else:
-                    match (dx, dy):
-                        case (-1, 0) | (1, 0):
-                            ii.add(nx)
-                        case (0, -1) | (0, 1):
-                            jj.add(ny)
+            # outer edges 
+            # check n & e & ne
+            if (x-1, y) not in self.tiles and (x, y+1) not in self.tiles and (x-1, y+1) not in self.tiles:
+                # print(x, y, " ne edge")
+                outeredges.append((x, y))
+            # check s & e & se
+            if (x+1, y) not in self.tiles and (x, y+1) not in self.tiles and (x+1, y+1) not in self.tiles:
+                # print(x, y, " se edge")
+                outeredges.append((x, y))
+            # check n & w & nw
+            if (x-1, y) not in self.tiles and (x, y-1) not in self.tiles and (x-1, y-1) not in self.tiles:
+                # print(x, y, " nw edge")
+                outeredges.append((x, y))
+            # check s & w & sw
+            if (x+1, y) not in self.tiles and (x, y-1) not in self.tiles and (x+1, y-1) not in self.tiles:
+                # print(x, y, " sw edge")
+                outeredges.append((x, y))
+                
+     
+            # innere ecken -> seite kein nachbar, diagonal aber 
+            # check e vs ne
+            if (x, y+1) not in self.tiles and ((x-1, y+1) in self.tiles):# or (x-1, y-1) in self.tiles):
+                # print(x, y, " e inner edge")
+                inneredges.append((x,y))
             
-        return len(ii) + len(jj)
+            # check w vs sw
+            if (x, y-1) not in self.tiles and ((x+1, y-1) in self.tiles):# or (x-1, y+1) in self.tiles):
+                # print(x, y, " w inner edge")
+                inneredges.append((x,y))
+            
+            # check n vs nw
+            if (x-1, y) not in self.tiles and ((x-1, y-1) in self.tiles):# or (x-1, y+1) in self.tiles):
+                # print(x, y, " n inner edge")
+                inneredges.append((x,y))
+            
+            # check s vs se
+            if (x+1, y) not in self.tiles and ((x+1, y+1) in self.tiles):# or (x+1, y-1) in self.tiles):
+                # print(x, y, " s inner edge")
+                inneredges.append((x,y))
+
+        edges = inneredges.copy()
+        # print(edges)
+        # for e in outeredges:
+        #     if e not in inneredges:
+        #         edges.append(e)
+        edges.extend(outeredges)        
+        
+        self.inner = inneredges
+        self.outer = outeredges
+        
+        # print("inner ",inneredges)
+        # print("outer ",outeredges)
+        
+        return len(edges)
     
     @property
     def new_price(self):
         return self.area * self.unique_sides
     
 m = parseInput('sample.dat')
-# m = parseInput()
+part1(m)        
+m = parseInput()
 part1(m)        
 
